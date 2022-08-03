0fe4e58148ad913ad7d14c4276c10733bbbebe18
patricia_tozi <ptrc.tozi@hotmail.com>
2022-08-03 00:38:50 -0300

    feat(web_scraper): include get_max_values global function to utils.py file

--- web_scraper/utils.py
+++ web_scraper/utils.py
@@ -17,3 +17,11 @@ def get_soup(url):
     soup = BeautifulSoup(html, 'html.parser')
 
     return soup
+
+def get_max_values(soup, tag_type, class_name, start_point, end_point):
+
+    html_limit = soup.find_all(tag_type,{"class": class_name})[0].get_text()
+
+    max_value = int((html_limit.strip())[len(html_limit.strip())-start_point:len(html_limit.strip())-end_point])
+    
+    return max_value
